f2c5db5a948f807c920f0c62172925e3a2c6eb8b
GiuseppeDot <giuseppegianni190@gmail.com>
2025-02-07 10:56:56 +0100

    Initial commit

---
+++ src/main/java/com/azienda/progetto_settimanaleS5L5/ProgettoSettimanaleS5L5Application.java
@@ -0,0 +1,13 @@
+package com.azienda.progetto_settimanaleS5L5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettimanaleS5L5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSettimanaleS5L5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/azienda/progetto_settimanaleS5L5/ProgettoSettimanaleS5L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azienda.progetto_settimanaleS5L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimanaleS5L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
